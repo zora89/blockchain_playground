5069fbe7ff34437ce1c73d0a0cd3e8e72e3fa34f
zora89 <zorawar.purohit@gmail.com>
2022-01-15 06:22:46 +0530

    added comments

--- eth/app.py
+++ eth/app.py
@@ -1,8 +1,19 @@
 from web3 import Web3
 infura_url = "https://mainnet.infura.io/v3/a31f23202f1f44568b85e40d13814ac2"
 web3 = Web3(Web3.HTTPProvider(infura_url))
+
+#testing for True connection state with eth blockchain
 print(web3.isConnected())
+
+#fetching for latest eth blockd data and saving to latest_block variable
 latest_block = web3.eth.get_block('latest')
+
+#Will print all info regarding latest eth block
 #print(latest_block)
+print(web3.eth.blockNumber)
+
+#acquiring a specific wallet balance
 balance_zp = web3.eth.getBalance("0x10F94d0C2C14Ff9C244940760CA0bd2dc445fDB3")
+
+#web3.fromWei -> helps covert Wei balance to required currency(positional argument)
 print(web3.fromWei(balance_zp, 'ether'))
